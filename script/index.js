1e5a44d099fdac200b6a9259978f3e31202f20df
Szymon Szczepanowski <s.szymon1127@gmail.com>
2021-05-14 14:27:33 +0200

    mobile

--- script/index.js
+++ script/index.js
@@ -9,11 +9,32 @@ const handleScroll = () => {
     scrollElements.forEach(el => {
         if(checkIfInView(el)) {
             el.classList.add('scrolled');
-            el.classList.remove('scroll');
         }
     })
 }
 
 window.addEventListener('scroll', () => {
     handleScroll();
+})
+
+const handleBorgier = () => {
+    if(!borgierBtn.classList.contains('active')){
+        borgierBtn.classList.add('active');
+        borgierNav.classList.add('show');
+        borgierNav.classList.remove('hide');
+        borgierNav.classList.remove('hidden');
+    } else {
+        borgierBtn.classList.remove('active');
+        borgierNav.classList.add('hide');
+        borgierNav.classList.add('hidden');
+        borgierNav.classList.remove('show');
+    }
+}
+
+const borgierBtn = document.querySelector('.borgier');
+const borgierNav = document.querySelector('.navbar-list-wrapper');
+const borgierNavItems = document.querySelectorAll('.navbar-borgier-list-item');
+borgierBtn.addEventListener('click', handleBorgier)
+borgierNavItems.forEach(el => {
+    el.addEventListener('click', handleBorgier)
 })
